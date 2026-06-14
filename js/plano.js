9ffb31a2a327c1d951107b72e93c7f1312b04e30
Ph <paulo170306@gmail.com>
2026-06-14 18:41:58 -0300

    feat: linhas do plano

--- js/plano.js
+++ js/plano.js
@@ -10,42 +10,54 @@ function loadPlane(){
     let vLines = 30;
     let hLines = 10;
 
-    drawHLines(ctx, backColor, 3, 0, 0, width, hLines, height/hLines);
-    drawVLines(ctx, backColor, 3, 0, 0, height, vLines, width/vLines);
+    drawHLines(ctx, 3, 0, 0, width, hLines, height/hLines);
+    drawVLines(ctx, 3, 0, 0, height, vLines, width/vLines);
     drawArrows(ctx, height/hLines, width/vLines, 10, height, width);
     ctx.font = "25px dogica serif";
     ctx.fillText(`y`, 29, 25);
     ctx.fillText(`x`, 1060, 334);
 }
 
-function drawVLines(canvas, color, width, xInicial, yInicial, yFinal, nLines, spacement){
-    canvas.strokeStyle = color;
+function drawVLines(canvas, width, xInicial, yInicial, yFinal, nLines, spacement){
     canvas.lineWidth = width;
     let currentX;
     for(let index = 0; index <= nLines; index++) {
+        canvas.strokeStyle = backColor;
         currentX = Math.floor(xInicial + ((index+1) * spacement)) + 0.5;
         canvas.beginPath();
             canvas.moveTo(currentX, yInicial);
             canvas.lineTo(currentX, yFinal);
             canvas.stroke();
         if(index < 28 && index > -1){
-            Numbers(canvas, index, currentX-3, 340);
+            canvas.strokeStyle = mainColor;
+            canvas.beginPath();
+                canvas.moveTo(currentX, 320);
+                canvas.lineTo(currentX, 320+15);
+                canvas.stroke();
+            Numbers(canvas, index, currentX-6, 350);
         }
     }
 }
 
-function drawHLines(canvas, color, width, xInicial, yInicial, xFinal, nLines, spacement){
-    canvas.strokeStyle = color;
+function drawHLines(canvas, width, xInicial, yInicial, xFinal, nLines, spacement){
     canvas.lineWidth = width;
     let currentY;
     for(let index = 0; index <= nLines; index++) {
+        canvas.strokeStyle = backColor;
         currentY = Math.floor(yInicial + ((index+1) * spacement)) + 0.5;
         canvas.beginPath();
             canvas.moveTo(xInicial, currentY);
             canvas.lineTo(xFinal, currentY);
             canvas.stroke();
         if(8-index < 8 && 8-index > -1){
-            Numbers(canvas, 8-index, 25, currentY+3);
+            if(currentY != 327.5){
+                canvas.strokeStyle = mainColor;
+                canvas.beginPath();
+                    canvas.moveTo(28, currentY);
+                    canvas.lineTo(46, currentY);
+                    canvas.stroke();
+            }
+            Numbers(canvas, 8-index, 10, currentY+3);
         }
     }
 }
@@ -88,6 +100,10 @@ function Numbers(canvas, char, x, y){
     if(char == 0){
         canvas.fillText(char, 24.5, 340.5);
     }else{
-        canvas.fillText(char, x, y);
+        if(char < 10){
+            canvas.fillText(`0${char}`, x, y);
+        }else{
+            canvas.fillText(char, x, y);
+        }
     }
 }
