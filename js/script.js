b1346ea2322639853e1b270771609212f48a2b10
guilherme.andrd <nickyandrade331@gmail.com>
2026-06-29 09:54:52 -0300

    fix: caixa de resposta não fecha com enter e fecha clicando fora dela

--- js/script.js
+++ js/script.js
@@ -2,17 +2,27 @@ const responseField = document.getElementById('responseField');
 const buttonVerify = document.getElementById('buttonVerify');
 const answerWaited = "";
 
+
 responseField.addEventListener("click", function (){
+    event.stopPropagation();
     responseField.style.width = "400px";
     responseField.style.height = "400px";
 })
 
-responseField.addEventListener("keydown", (event) => {
-    if(event.key === "Enter"){
+document.addEventListener("click", function(){
+    if(responseField.style.width == "400px" && responseField.style.height == "400px"){
         responseField.style.width = "200px";
         responseField.style.height = "70px";
     }
 })
+
+
+/*responseField.addEventListener("keydown", (event) => {
+    if(event.key === "Enter"){
+        responseField.style.width = "200px";
+        responseField.style.height = "70px";
+    }
+})*/
 
 buttonVerify.addEventListener("click", () => {
     if(responseField.innerText() === answerWaited){
